778ebe8d05d4af92d257de7f10150a68ffa94361
Femi Akinyemi <santosfemakin@yahoo.com>
2021-03-27 23:07:32 +0100

    Added Ellipseimages and styled them

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -4,7 +4,29 @@
         return (
             <div>
                 <footer>
-                    <div className="footer_div">This is the footer</div>
+                    <div className="footer_div">
+                        <h1 className="footer_brand">ESEOWIE</h1>
+                        <div>
+                            <p className="footer_paragraph">
+                            Political Career
+                            </p>
+                            <p className="footer_paragraph">
+                            Professional Career
+                            </p>
+                        </div>
+                        <div>
+                            <p className="footer_paragraph">
+                            Academics
+                            </p>
+                            <p className="footer_paragraph">
+                            Contact
+                            </p>
+                        </div>
+                    </div>
+                    <div className="footer_copyright">
+                        <p className="copy">Copyright © 2021 Ese Owie</p>
+                        <p className="copy">Designed and Developed by Techinnover Analytics</p>
+                    </div>
                 </footer>
             </div>
         )
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Ellipse2 from '../assets/Images/Ellipse2.png';
+import Ellipse1 from '../assets/Images/Ellipse1.png';
 
 export default function HomePage() {
     return (
         <div className=" container homepage">
             <h1>This is the Landing page</h1>
+            <img src={Ellipse2} alt="Ellipse2" className="ellipse2"/>
+            <img src={Ellipse1} alt="Ellipse1" className="ellipse1"/>
         </div>
     )
 }
